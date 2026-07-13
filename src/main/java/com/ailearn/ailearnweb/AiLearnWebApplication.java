7406b1fe5e9f823bba50b13d7d7c1d775cc02523
范烨 <2876479587@qq.com>
2026-07-13 15:51:08 +0800

    初始化

---
+++ src/main/java/com/ailearn/ailearnweb/AiLearnWebApplication.java
@@ -0,0 +1,13 @@
+package com.ailearn.ailearnweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLearnWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLearnWebApplication.class, args);
+	}
+
+}
